a6ff04f224bc494c219426a879dda7287657f135
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-12 13:38:09 -0700

    Added an adaptive UI package containing color utilities, design tokens, and patterns (#39)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This work adds a private package to assist in revving the CLI and it includes the following work for use by the components:
    - Design tokens
    - Color utilities
    - Component style patterns
    
    It also speeds up the build by using the static generated storybook files to run on playwright.
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    Continues work on #31
    
    ## 👩‍💻 Reviewer Notes
    
    <!---
    Provide some notes for reviewers to help them provide targeted feedback and testing.
    -->
    The files should be very similar to what was in `@microsoft/fast-components`, with a few fixes to integrate into the CLI.
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Begin adding components

--- packages/adaptive-ui/playwright.config.ts
+++ packages/adaptive-ui/playwright.config.ts
@@ -3,11 +3,6 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
     testDir: "dist",
     testMatch: "**/?(*.)+(spec).+(js)",
-    webServer: {
-        command: "npm run serve:storybook",
-        port: 3000,
-        reuseExistingServer: !process.env.CI,
-    }
 };
 
 export default config;
