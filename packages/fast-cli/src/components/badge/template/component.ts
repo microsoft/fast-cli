ae2ed39f383adf1c9dd0d8103d6fceef2a84027f
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-13 14:15:44 -0700

    move compose to the define file

--- packages/fast-cli/src/components/badge/template/component.ts
+++ packages/fast-cli/src/components/badge/template/component.ts
@@ -2,15 +2,8 @@ import type { ComponentTemplateConfig } from "../../../utilities/template";
 
 export default (config: ComponentTemplateConfig): string =>
 `import { Badge } from "@microsoft/fast-foundation";
-import definition from "./${config.tagName}.definition.js";
 
 /**
- * A function that returns a {@link @microsoft/fast-foundation#Badge} registration for configuring the component with a DesignSystem.
- * Implements {@link @microsoft/fast-foundation#badgeTemplate}
- *
- *
- * @public
- * @remarks
- * Generates HTML Element: \`<${config.tagName}>\`
+ * A class derived from the Badge foundation component
  */
-export default Badge.compose(definition);`
+export default class ${config.className} extends Badge {};`
--- packages/fast-cli/src/components/badge/template/define.ts
+++ packages/fast-cli/src/components/badge/template/define.ts
@@ -1,1 +1,7 @@
-export default (): string => `import "./component.js";`;
+import type { ComponentTemplateConfig } from "../../../utilities/template";
+
+export default (config: ComponentTemplateConfig): string =>
+`import ${config.className} from "./${config.tagName}.js";
+import definition from "./${config.tagName}.definition.js";
+
+${config.className}.compose(definition);`;
