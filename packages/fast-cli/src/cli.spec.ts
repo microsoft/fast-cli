57706823ae723171450cff5612acec5f8eabdd22
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-20 09:59:19 -0700

    remove dupe test

--- packages/fast-cli/src/cli.spec.ts
+++ packages/fast-cli/src/cli.spec.ts
@@ -470,49 +470,6 @@ test.describe("CLI", () => {
                 ).not.toThrow();
             });
         });
-        test.describe("number-field", () => {
-            test.beforeAll(() => {
-                setup(tempDir, tempComponentDir);
-                execSync(`cd ${tempDir} && npm run fast:init`);
-                setup(tempDir, tempComponentDir);
-                execSync(`cd ${tempDir} && npm run fast:add-foundation-component:number-field`);
-            });
-            test.afterAll(() => {
-                teardown(tempDir, tempComponentDir);
-            });
-            test("should copy files from the template", () => {
-                let files: Array<string> = [];
-
-                function testGeneratedFiles(folderName: string) {
-                    const tempDirContents = fs.readdirSync(path.resolve(tempDir, "src/components/test-component", folderName));
-                    const tempDirContentsWithFileTypes = fs.readdirSync(path.resolve(tempDir, "src/components/test-component", folderName), {
-                        withFileTypes: true
-                    });
-
-                    for (let i = 0, contentLength = tempDirContents.length; i < contentLength; i++) {
-                        if (tempDirContentsWithFileTypes[i].isDirectory()) {
-                            testGeneratedFiles(tempDirContents[i]);
-                        } else {
-                            files.push(
-                                folderName
-                                    ? `${folderName}/${tempDirContents[i]}`
-                                    : tempDirContents[i]
-                            );
-                        }
-                    }
-                }
-                
-                testGeneratedFiles("");
-                expect(files).toEqual(expectedGeneratedComponentTemplateFiles);
-            });
-            test("should be able to run the build", () => {
-                expect(
-                    () => {
-                        execSync(`cd ${tempDir} && npm run build`);
-                    }
-                ).not.toThrow();
-            });
-        });
         test.describe("search", () => {
             test.beforeAll(() => {
                 setup(tempDir, tempComponentDir);
