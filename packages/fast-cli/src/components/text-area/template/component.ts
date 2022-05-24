21bfc24fa2c921585f1fbf1f7d57e06114951dc2
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-23 19:40:33 -0700

    Add specific appearance styling

--- packages/fast-cli/src/components/text-area/template/component.ts
+++ packages/fast-cli/src/components/text-area/template/component.ts
@@ -1,9 +1,26 @@
 import type { ComponentTemplateConfig } from "../../../utilities/template";
 
 export default (config: ComponentTemplateConfig): string =>
-`import { TextArea } from "@microsoft/fast-foundation";
+`import { attr } from "@microsoft/fast-element";
+import { TextArea } from "@microsoft/fast-foundation";
+
+/**
+ * Text area appearances
+ * @public
+ */
+export type TextAreaAppearance = "filled" | "outline";
 
 /**
  * A class derived from the TextArea foundation component
  */
-export class ${config.className} extends TextArea {};`
+export class ${config.className} extends TextArea {
+    /**
+     * The appearance of the element.
+     *
+     * @public
+     * @remarks
+     * HTML Attribute: appearance
+     */
+    @attr
+    public appearance: TextAreaAppearance = "outline";
+};`
