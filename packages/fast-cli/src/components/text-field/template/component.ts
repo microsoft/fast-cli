ed9a0b3666385678a622e081a852d8aa2d959c42
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-23 19:38:47 -0700

    Add the attr to the class

--- packages/fast-cli/src/components/text-field/template/component.ts
+++ packages/fast-cli/src/components/text-field/template/component.ts
@@ -1,9 +1,26 @@
 import type { ComponentTemplateConfig } from "../../../utilities/template";
 
 export default (config: ComponentTemplateConfig): string =>
-`import { TextField } from "@microsoft/fast-foundation";
+`import { attr } from "@microsoft/fast-element";
+import { TextField } from "@microsoft/fast-foundation";
+
+/**
+ * Text field appearances
+ * @public
+ */
+export type TextFieldAppearance = "filled" | "outline";
 
 /**
  * A class derived from the TextField foundation component
  */
-export class ${config.className} extends TextField {};`
+export class ${config.className} extends TextField {
+    /**
+     * The appearance of the element.
+     *
+     * @public
+     * @remarks
+     * HTML Attribute: appearance
+     */
+    @attr
+    public appearance: TextFieldAppearance = "outline";
+};`
