a4d925e33aa83676d17c932b9688f5040604861e
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-23 19:30:22 -0700

    Add specific class logic

--- packages/fast-cli/src/components/toolbar/template/component.ts
+++ packages/fast-cli/src/components/toolbar/template/component.ts
@@ -1,9 +1,27 @@
 import type { ComponentTemplateConfig } from "../../../utilities/template";
 
 export default (config: ComponentTemplateConfig): string =>
-`import { Toolbar } from "@microsoft/fast-foundation";
+`import { composedParent, Toolbar } from "@microsoft/fast-foundation";
+import type { Swatch } from "@microsoft/adaptive-ui/dist/dts/color/swatch.d.js"
+import { fillColor, neutralFillLayerRecipe } from "@microsoft/adaptive-ui";
 
 /**
  * A class derived from the Toolbar foundation component
  */
-export class ${config.className} extends Toolbar {};`
+export class ${config.className} extends Toolbar {
+    connectedCallback() {
+        super.connectedCallback();
+
+        const parent = composedParent(this);
+
+        if (parent) {
+            fillColor.setValueFor(
+                this,
+                (target: HTMLElement): Swatch =>
+                    neutralFillLayerRecipe
+                        .getValueFor(target)
+                        .evaluate(target, fillColor.getValueFor(parent))
+            );
+        }
+    }
+};`
