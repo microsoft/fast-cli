78b8615561c284b40b60b1b86e039e857aacdad1
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-23 19:26:08 -0700

    Added some missing class logic to components that have deviated from foundation

--- packages/fast-cli/src/components/number-field/template/component.ts
+++ packages/fast-cli/src/components/number-field/template/component.ts
@@ -2,7 +2,25 @@ import type { ComponentTemplateConfig } from "../../../utilities/template";
 
 export default (config: ComponentTemplateConfig): string =>
 `import { NumberField } from "@microsoft/fast-foundation";
+import { attr } from "@microsoft/fast-element";
+
+/**
+ * Number field appearances
+ * @public
+ */
+export type NumberFieldAppearance = "filled" | "outline";
+
 /**
  * A class derived from the NumberField foundation component
  */
-export class ${config.className} extends NumberField {};` 
+export class ${config.className} extends NumberField {
+    /**
+     * The appearance of the element.
+     *
+     * @public
+     * @remarks
+     * HTML Attribute: appearance
+     */
+    @attr
+    public appearance: NumberFieldAppearance = "outline";
+};` 
