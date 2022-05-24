18bf84170f0daf31da465e92cc2d12d7443993ed
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-24 11:17:32 -0700

    Added the text-field component (#58)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    Added the text-field foundation component.
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    - Continues work on #31
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [x] I have added tests for my changes.
    - [x] I have tested my changes.
    - [x] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Continue adding foundation components

--- packages/fast-cli/src/components/options.ts
+++ packages/fast-cli/src/components/options.ts
@@ -12,5 +12,6 @@ export const availableTemplates = [
     "progress-ring",
     "search",
     "switch",
+    "text-field",
     "toolbar",
 ]
---
+++ packages/fast-cli/src/components/text-field/template/component.template.ts
@@ -0,0 +1,12 @@
+import type { ComponentTemplateConfig } from "../../../utilities/template";
+
+export default (config: ComponentTemplateConfig): string => `
+import { TextField, textFieldTemplate, FoundationElementTemplate } from "@microsoft/fast-foundation";
+import type { ViewTemplate } from "@microsoft/fast-element";
+
+/**
+ * The template for ${config.className} component.
+ * @public
+ */
+export const template: FoundationElementTemplate<ViewTemplate<TextField>> = textFieldTemplate;
+`;
