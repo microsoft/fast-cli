eda847c32c81d981593ead69ac6d768b6c59b066
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-05-25 09:52:48 -0700

    Added the tooltip foundation component (#61)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This adds the tooltip foundation component.
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    - Continues work on #31
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [x] I have added tests for my changes.
    - [x] I have tested my changes.
    - [x] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Continue adding foundation components

---
+++ packages/fast-cli/src/components/tooltip/template/README.ts
@@ -0,0 +1,8 @@
+import type { ComponentTemplateConfig } from "../../../utilities/template";
+
+export default (config: ComponentTemplateConfig): string => `
+# ${config.className}
+
+An implementation of a [tooltip](https://w3c.github.io/aria-practices/#tooltip) web-component.
+
+For more information on the building blocks used to create this component, please refer to https://www.fast.design/docs/components/tooltip`;
