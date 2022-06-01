c9d680d1680a1bca5315db978412cc64f34b6859
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-06-01 15:33:48 -0700

    Added the avatar component (#64)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This change adds the avatar foundation component.
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    Continues work on #31
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [x] I have added tests for my changes.
    - [x] I have tested my changes.
    - [x] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Continue work on #31

---
+++ packages/fast-cli/src/components/avatar/template/fixture.ts
@@ -0,0 +1,55 @@
+import type { ComponentTemplateConfig } from "../../../utilities/template";
+
+export default (config: ComponentTemplateConfig): string =>
+`<style>
+${config.tagName} {
+    --avatar-fill-accent-primary: #cf4073;
+    --avatar-fill-accent-secondary: #0078d4;
+    --avatar-color-foo: hsl(0, 0%, 100%);
+    --avatar-color-bar: grey;
+    --avatar-text-ratio: 3;
+}
+
+.container {
+    padding: 1em;
+}
+</style>
+<div class="container">
+    <h1>${config.className}</h1>
+
+    <h2>Circle shaped avatar</h2>
+
+    <h3>Circle avatar with image</h3>
+    <${config.tagName}
+        alt="Annie's profile image"
+        link="#"
+        shape="circle"
+        fill="accent-primary"
+        color="bar"
+    >
+        <img class="image" slot="media" src="https://via.placeholder.com/32" />
+    </${config.tagName}>
+
+    <h3>Circle avatar with text content</h3>
+    <${config.tagName}
+        alt="Carlos's profile image"
+        link="#"
+        shape="circle"
+        fill="accent-primary"
+        color="foo"
+    >
+        CR
+    </${config.tagName}>
+
+    <h2>Square shaped avatar</h2>
+
+    <h3>Square avatar with image</h3>
+    <${config.tagName}
+        src="https://via.placeholder.com/32"
+        alt="Fang's profile image"
+        link="#"
+        shape="square"
+        fill="accent-secondary"
+    ></${config.tagName}>
+
+</div>`;
