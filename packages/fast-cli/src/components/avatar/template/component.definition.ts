c64636042099637bed97626d07fb228395ca8ac0
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-06-01 13:45:56 -0700

    Add component to available components

--- packages/fast-cli/src/components/avatar/template/component.definition.ts
+++ packages/fast-cli/src/components/avatar/template/component.definition.ts
@@ -3,14 +3,16 @@ import type { ComponentTemplateConfig } from "../../../utilities/template";
 export default (config: ComponentTemplateConfig): string => 
 `import { template } from "./${config.tagName}.template.js";
 import { styles } from "./${config.tagName}.styles.js";
+import type { ${config.className} } from "./${config.tagName}.js";
+import { html, when } from "@microsoft/fast-element";
 import { Avatar } from "@microsoft/fast-foundation";
 
 export const definition = {
     baseName: "${config.tagName}",
-    baseClass: ,
+    baseClass: Avatar,
     template,
     styles,
-    media: html<Avatar>\`
+    media: html<${config.className}>\`
         \${when(
             x => x.imgSrc,
             html\`
--- packages/fast-cli/src/components/options.ts
+++ packages/fast-cli/src/components/options.ts
@@ -1,4 +1,5 @@
 export const availableTemplates = [
+    "avatar",
     "badge",
     "blank",
     "card",
