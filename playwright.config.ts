30ea0c552067f8fea1be2c17ce7ac1cdfb877d42
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-06-16 15:08:19 -0700

    do not run webserver for the cli test

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,11 +5,6 @@ const config: PlaywrightTestConfig = {
     retries: process.env.CI ? 2 : 0,
     testMatch: "**/?(*.)+(spec).+(js)",
     retries: process.env.CI ? 2 : 0,
-    webServer: {
-        command: "npm run serve:storybook",
-        port: 3000,
-        reuseExistingServer: !process.env.CI,
-    }
 };
 
 export default config;
