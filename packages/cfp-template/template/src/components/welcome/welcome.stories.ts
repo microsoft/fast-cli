002ed727aa8f344280b0a369044e6e81b557d45b
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-06-20 09:52:32 -0700

    Correct version numbers and welcome component definition

--- packages/cfp-template/template/src/components/welcome/welcome.stories.ts
+++ packages/cfp-template/template/src/components/welcome/welcome.stories.ts
@@ -1,5 +1,14 @@
 import Template from "./fixtures/base.html";
 import "./define.js";
+import { DesignSystem } from "@microsoft/fast-foundation";
+import { welcome } from "./define.js";
+import { designSystem } from "../../design-system";
+
+DesignSystem.getOrCreate().withPrefix(
+    designSystem.prefix
+).register(
+    welcome()
+);
 
 export default {
     title: "welcome",
