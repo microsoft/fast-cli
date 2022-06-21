b4f6324582c38d62145e10311d97a80604d6ffb7
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-06-21 08:17:25 -0700

    Resolve an eslint issue

--- packages/cfp-template/template/src/components/welcome/welcome.stories.ts
+++ packages/cfp-template/template/src/components/welcome/welcome.stories.ts
@@ -1,8 +1,8 @@
+import { DesignSystem } from "@microsoft/fast-foundation";
+import { designSystem } from "../../design-system.js";
 import Template from "./fixtures/base.html";
 import "./define.js";
-import { DesignSystem } from "@microsoft/fast-foundation";
 import { welcome } from "./define.js";
-import { designSystem } from "../../design-system";
 
 DesignSystem.getOrCreate().withPrefix(
     designSystem.prefix
