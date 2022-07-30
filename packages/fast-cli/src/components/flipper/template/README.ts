928a00feafb06d1554540137a42af89436a1346c
onrails <walid.bouguima@gmail.com>
2022-07-30 16:18:09 +0100

    fix(docs): flipper reference link points to card
    I think that the reference link to "read more" should point to the flipper component URI not to the card component.

--- packages/fast-cli/src/components/flipper/template/README.ts
+++ packages/fast-cli/src/components/flipper/template/README.ts
@@ -3,4 +3,4 @@ import { mdTemplate } from "../../../cli.js";
 export default mdTemplate`# ${c => c.className}
 The flipper component is most often used to page through blocks of content or collections of UI elements.
 
-For more information on the building blocks used to create this component, please refer to https://www.fast.design/docs/components/card`;
+For more information on the building blocks used to create this component, please refer to https://www.fast.design/docs/components/flipper`;
