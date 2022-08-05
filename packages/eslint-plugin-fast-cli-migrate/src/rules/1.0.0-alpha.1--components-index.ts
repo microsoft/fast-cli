ad50528062707dedeaa9da8658717602b9f32a43
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-08-05 15:30:55 -0700

    eslint

--- packages/eslint-plugin-fast-cli-migrate/src/rules/1.0.0-alpha.1--components-index.ts
+++ packages/eslint-plugin-fast-cli-migrate/src/rules/1.0.0-alpha.1--components-index.ts
@@ -1,8 +1,8 @@
+import path from "path";
 import { Rule } from "eslint";
 import type {
     ImportDeclaration,
 } from "estree";
-import path from "path";
 
 export const meta = {
     type: "problem",
